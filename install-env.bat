00cd20b6c70a8c4c33fe33a10f0e45d7f6d40cc4
arthur <arthur.oliveira@autaza.com>
2024-08-27 22:54:27 -0300

    instalação

---
+++ install-env.bat
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+pip install torch==1.10.0+cu113 -f https://download.pytorch.org/whl/torch/
+pip install torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/torchaudio/
+pip install torchvision==0.11.1+cu113 -f https://download.pytorch.org/whl/torchvision/
